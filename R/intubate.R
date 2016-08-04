c4d4d33b9546b775a0789129fd2b40645d109274
Roberto Bertolusso <rbertolusso@gmail.com>
2016-08-03 23:01:52 -0500

    Minor problem corrected

--- R/intubate.R
+++ R/intubate.R
@@ -18,46 +18,174 @@
 function_data <- function(data, ...) data
 
 ## Interface function
-ntbt_function_data <- function(data, ...) {
-  preCall <- match.call(expand.dots = FALSE)
+ntbt_function_data <-
   
-  Call <- match.call(expand.dots = TRUE)
-  Call[[1]] <- get_calling_name("ntbt", as.character(Call[[1]]))
-  Call[[2]] <- as.name("data")
+  ## e1071
+  ntbt_svm <-
   
-  is_formula <- try(inherits(as.formula(as.character(preCall$...[[1]])),
-                             "formula"), silent = TRUE)
-  is_formula <- !(class(is_formula)[[1]] == "try-error")
+  ## gam
+  ntbt_gam <-
   
-  if (is_formula) {
-    ## cat("Formula\n")
-    Call[2:3] <- Call[3:2]                ## Switch parameters
-    names(Call)[2:3] <- c("", "data")     ## Leave formula unnamed
-    print(Call)
-    ret <- try(eval(Call), silent = TRUE) ## Let's make it more robust
-    if (class(ret)[[1]] == "try-error") { ## Maybe data has other name
-      names(Call)[[3]] <- ""              ## Leave data unnamed
+  ## gbm
+  ntbt_gbm <-
+  
+  ## graphics
+  ntbt_boxplot <-
+  ntbt_cdplot <-
+  ntbt_coplot <-
+  ntbt_mosaicplot <-
+  ntbt_pairs <-
+  ntbt_plot <-
+  ntbt_spineplot <-
+  ntbt_sunflowerplot <-
+  ntbt_stripchart <-
+  ntbt_text <-
+  
+  ## lattice
+  ntbt_barchart <-
+  ntbt_cloud <-
+  ntbt_bwplot <-
+  ntbt_contourplot <-
+  ntbt_dotplot <-
+  ntbt_densityplot <-
+  ntbt_histogram <-
+  ntbt_levelplot <-
+  ntbt_oneway <-
+  ntbt_parallelplot <-
+  ntbt_qq <-
+  ntbt_qqmath <-
+  ntbt_splom <-
+  ntbt_stripplot <-
+  ntbt_wireframe <-
+  ntbt_xyplot <-
+  
+  ## leaps
+  ntbt_regsubsets <-
+  ntbt_tmd <-
+  
+  ## lfe
+  ntbt_felm <-
+  
+  ## MASS
+  ntbt_corresp <-
+  ntbt_glm.nb <-
+  ntbt_lda <-
+  ntbt_lm.gls <-
+  ntbt_lm.ridge <-
+  ntbt_loglm <-
+  ntbt_logtrans <-
+  ntbt_polr <-
+  ntbt_qda <-
+  ntbt_rlm <-
+  
+  ## nlme
+  ntbt_gls <-
+  ntbt_lme <-
+  ntbt_lmList <-
+  ntbt_nlme <-
+  ntbt_nlsList <-
+  
+  ## nnet
+  ntbt_multinom <-
+  ntbt_nnet <-
+  
+  ## pls
+  ntbt_cppls <-
+  ntbt_mvr <-
+  ntbt_pcr <-
+  ntbt_plsr <-
+  
+  ## randomForest
+  ntbt_randomForest <-
+  
+  ## rpart
+  ntbt_rpart <-
+  
+  ## stats
+  ntbt_aggregate <-
+  ntbt_alias <-
+  ntbt_ansari.test <-
+  ntbt_aov <-
+  ntbt_bartlett.test <-
+  ntbt_cor.test <-
+  ntbt_fligner.test <-
+  ntbt_friedman.test <-
+  ntbt_ftable <-
+  ntbt_getInitial <-
+  ntbt_glm <-
+  ntbt_kruskal.test <-
+  ntbt_lm <-
+  ntbt_loess <-
+  ntbt_lqs <-
+  ntbt_model.frame <-
+  ntbt_model.matrix <-
+  ntbt_mood.test <-
+  ntbt_nls <-
+  ntbt_oneway.test <-
+  ntbt_ppr <-
+  ntbt_prcomp <-
+  ntbt_princomp <-
+  ntbt_quade.test <-
+  ntbt_replications <-
+  ntbt_t.test <-
+  ntbt_var.test <-
+  ntbt_wilcox.test <-
+  ntbt_xtabs <-
+  
+  ## survival
+  ntbt_cch <-
+  ntbt_coxph <-
+  ntbt_finegray <-
+  ntbt_pyears <-
+  ntbt_survConcordance <-
+  ntbt_survexp <-
+  ntbt_survfit <-
+  ntbt_survSplit <-
+  
+  ## tree
+  ntbt_tree <-
+  
+  ## Interface function
+  function(data, ...) {
+    preCall <- match.call(expand.dots = FALSE)
+    
+    Call <- match.call(expand.dots = TRUE)
+    Call[[1]] <- get_calling_name("ntbt", as.character(Call[[1]]))
+    Call[[2]] <- as.name("data")
+    
+    is_formula <- try(inherits(as.formula(as.character(preCall$...[[1]])),
+                               "formula"), silent = TRUE)
+    is_formula <- !(class(is_formula)[[1]] == "try-error")
+    
+    if (is_formula) {
+      ## cat("Formula\n")
+      Call[2:3] <- Call[3:2]                ## Switch parameters
+      names(Call)[2:3] <- c("", "data")     ## Leave formula unnamed
       print(Call)
-      ret <- eval(Call)                   ## Retry
+      ret <- try(eval(Call), silent = TRUE) ## Let's make it more robust
+      if (class(ret)[[1]] == "try-error") { ## Maybe data has other name
+        names(Call)[[3]] <- ""              ## Leave data unnamed
+        print(Call)
+        ret <- eval(Call)                   ## Retry
+      }
+    } else if (length(preCall$...) > 0)  {
+      ## cat("Rest of cases\n")
+      print(Call)
+      ret <- with(data, eval(Call[-2]))     ## Need to remove data [-2]
+    } else  {
+      ## cat("No arguments other that data\n")
+      names(Call)[[2]] <- ""                ## Leave data unnamed
+      print(Call)
+      ret <- eval(Call)
     }
-  } else if (length(preCall$...) > 0)  {
-    ## cat("Rest of cases\n")
-    print(Call)
-    ret <- with(data, eval(Call[-2]))     ## Need to remove data [-2]
-  } else  {
-    ## cat("No arguments other that data\n")
-    names(Call)[[2]] <- ""                ## Leave data unnamed
-    print(Call)
-    ret <- eval(Call)
+    if (!is.null(ret)) {
+      if (withVisible(ret)$visible)
+        return (ret)
+      else
+        data <- ret
+    }
+    invisible(data)
   }
-  if (!is.null(ret)) {
-    if (withVisible(ret)$visible)
-      return (ret)
-    else
-      data <- ret
-  }
-  invisible(data)
-}
 
 ## Name checking (internal)
 get_calling_name <- function(prefix, full_name) {
