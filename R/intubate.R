c1df8221b45d69c1a48bc74b11011d3707260726
Roberto Bertolusso <rbertolusso@gmail.com>
2016-08-05 10:43:23 -0500

    Formula case moved to a separate internal function

--- R/intubate.R
+++ R/intubate.R
@@ -153,48 +153,55 @@ intubate <-
     if (length(preCall$...) == 0)  {
       ## cat("No arguments other than data\n")
       ## print(Call)
-      ret <- eval(Call)
+      result <- eval(Call)
     } else if (there_are_formulas(preCall$...)) {
-      ## This special case for formulas is, at least for now, needed because
-      ## "Rest of cases" below does not know how to manage cases with "." in
-      ## a formula (and the called function neither because only sees the variables
-      ## inside the data, not the data itself). An alternative could be to have in
-      ## "Rest of cases" some sort of "stats::model.matrix" call (of which I have
-      ## not clue how to implement), for *all* the formulas that have a ".", so maybe
-      ## the end result will be even more involved and this is as good as we can do.
       ## cat("Formula\n")
-      signal_error <- TRUE
-      ## Let's have "data" take a walk until it finds its place in the world,
-      ## as functions are supposed to check if unnamed parameters are sent
-      ## in the wrong order (Right?).
-      for (par in 3:length(names(Call))) {
-        Call[(par-1):par] <- Call[par:(par-1)]        ## Switch parameters
-        names(Call)[(par-1):par] <- names(Call)[par:(par-1)]  ## and names
-        ## print(Call)
-        ret <- try(eval(Call, envir = parent.frame()),  ## See if it flies
-                   silent = TRUE)
-        if (class(ret)[[1]] != "try-error") {          ## Did. We are done
-          signal_error <- FALSE
-          break
-        }
-      }
-      if (signal_error) {          ## Parameters exhausted and still error
-        print(Call)                ## Show call of last attempt
-        stop(ret)                  ## Give up
-      }
+      result <- process_formula_case(Call, use_envir = parent.frame())      
     } else  {
       ## cat("Rest of cases\n")
       ## print(Call)
-      ret <- with(data, eval(Call[-2]))    ## Remove "data" [-2] before calling
+      result <- with(data, eval(Call[-2]))    ## Remove "data" [-2] before calling
     }
-    if (!is.null(ret)) {
-      if (withVisible(ret)$visible)
-        return (ret)
+    if (!is.null(result)) {
+      if (withVisible(result)$visible)
+        return (result)
       else
-        data <- ret
+        data <- result
     }
     invisible(data)
   }
+
+
+process_formula_case <- function(Call, use_envir) {
+  ## This special case for formulas is, at least for now, needed because
+  ## "Rest of cases" below does not know how to manage cases with "." in
+  ## a formula (and the called function neither because only sees the variables
+  ## inside the data, not the data itself). An alternative could be to have in
+  ## "Rest of cases" some sort of "stats::model.matrix" call (of which I have
+  ## not clue how to implement), for *all* the formulas that have a ".", so maybe
+  ## the end result will be even more involved and this is as good as we can do.
+  signal_error <- TRUE
+  ## Let's have "data" take a walk until it finds its place in the world,
+  ## as functions are supposed to check if unnamed parameters are sent
+  ## in the wrong order (Right?).
+  for (par in 3:length(names(Call))) {
+    Call[(par-1):par] <- Call[par:(par-1)]        ## Switch parameters
+    names(Call)[(par-1):par] <- names(Call)[par:(par-1)]  ## and names
+    print(Call)
+    result <- try(eval(Call, envir = use_envir),  ## See if it flies
+                  silent = TRUE)
+    if (class(result)[[1]] != "try-error") {          ## Did. We are done
+      signal_error <- FALSE
+      break
+    }
+  }
+  if (signal_error) {          ## Parameters exhausted and still error
+    print(Call)                ## Show call of last attempt
+    stop(result)                  ## Give up
+  }
+  result
+}
+
 
 ## Determine if there is a formula (internal)
 there_are_formulas <- function(par_list) {
